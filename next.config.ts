6a1fb5186031b8e140d6a87c63a758431561e4f0
NANO1903 <fernando.beristain.alvarez@gmail.com>
2025-11-16 12:41:40 -0600

    fix: Patch Parsing and Compilation Errors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', '192.168.0.*', '127.0.0.1'],
 };
 
 export default nextConfig;
