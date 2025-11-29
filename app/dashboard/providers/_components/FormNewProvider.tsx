1ad5ff734df81e7b22f4ccbeceeda9d6ddda4e35
NANO1903 <fernando.beristain.alvarez@gmail.com>
2025-11-28 22:38:54 -0600

    fix: Form Title Rename

--- app/dashboard/providers/_components/FormNewProvider.tsx
+++ app/dashboard/providers/_components/FormNewProvider.tsx
@@ -18,7 +18,7 @@ export default async function FormNewProvider() {
     // const dataProvider: Provider = await response.json();
     return (
         <form action={createProvider} className="bg-orange-400 py-2 px-2 flex flex-col gap-6 w-full rounded-lg">
-            <h1 className="text-3xl text-white text-center font-extrabold">Editar Manager</h1>
+            <h1 className="text-3xl text-white text-center font-extrabold">Agregar Proveedor</h1>
             <Input isRequired label="Nombre" placeholder="Bimbo" name="providerName" />
             <Input isRequired label="Correo" placeholder="correo@dominio.com" name="providerEmail" />
             <Input isRequired label="Número de Teléfono" placeholder="4571236448" name="providerPhoneNumber" />
